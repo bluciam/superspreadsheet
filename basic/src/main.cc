8cdc86ae646cf32a4b4ccc31a1536e1d1750a059
John Plaice @ lupus <plaice@cse.unsw.edu.au>
2010-08-10 16:52:47 +0530

    Initial commit once the files have been created.

--- basic/src/main.cc
+++ basic/src/main.cc
@@ -1,23 +1,48 @@
-#include <gtkmm/main.h>
-#include "spreadsheet.h"
-
 int main(int argc, char *argv[])
 {
-//  Gtk::Main::init_gtkmm_internals();
 
-  Gtk::Main* kit = new Gtk::Main(argc, argv);
-  spreadsheet* sss = new spreadsheet;
+  for (int i = 0 ; i != row_range  ; ++i)
+  {
+     for (int j = 0 ; j != col_range ; ++j)
+     {
+       std::string s;
+       std::stringstream out;
+       out << (i+h_min) << " " << (j+v_min);
+       s = out.str();
 
-  //Shows the spreadsheet window and returns when it is closed.
-  Gtk::Main::run(*sss);
-  delete sss;
-  delete kit;
-
-//  Gtk::Main kit(argc, argv);
-//  spreadsheet sss;
-//  Gtk::Main::run(sss);
-
-
+       Glib::ustring cell = s; 
+       (*label).set_label(cell); 
+       (*frame).add(*label);
+       (*table).attach(*frame, i+1, i+2, j+1, j+2, Gtk::FILL, Gtk::FILL);
+       std::cout << "expression = " << expression << std::endl;
+       std::stringstream newout;
+       newout << expression;
+       newout << " @ [";
+       newout << *h_dim << ":" << (i+h_min) << ", ";
+       newout << *v_dim << ":" << (j+v_min);
+       for (std::map<Glib::ustring,int>::iterator mit = tuples.begin();
+            mit != tuples.end(); ++mit)
+       {
+         if (mit->first != *h_dim && mit->first != *v_dim)
+         {
+	   newout << ", " << mit->first << ":" << mit->second;
+         }
+       }
+       newout << "]";
+       std::cout << newout.str() << std::endl;
+       std::u32string tuple32;
+       for (std::string::iterator it = newout.str().begin();
+            it != newout.str().end(); ++it)
+       {
+         int i = *it;
+         char32_t c = i;
+         tuple32.push_back(c);
+       }
+       tuple32.push_back(0);
+       TL::HD* cellContext = traductor.translate_expr(tuple32);
+       //std::cout << newout.str() << std::endl;
+     }
+  }
   return 0;
 }
 
